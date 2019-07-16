8aa91c4a36a891a91f7197eb57ded530e7dedd88
minjiexm <minjiexm@hotmail.com>
2019-07-16 23:59:47 +0800

    add apb_agent tb

---
+++ apb_agent_pkg/apb_agent_pkg.sv
@@ -0,0 +1,14 @@
+
+`include "apb_if.svh"
+
+
+package apb_agent_pkg;
+
+import uvm_pkg::*;
+
+`include "apb_rw.svh"
+`include "apb_sequences.svh"
+`include "apb_driver_seq_mon.svh"
+`include "apb_agent_env_config.svh"
+
+endpackage
